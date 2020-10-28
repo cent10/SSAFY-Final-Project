30e6b2cd5206616fcec185f4d1902802e5e91229
박준수 <junsoodark@naver.com>
2020-10-28 15:55:17 +0900

    [add] S03P31A210-14 유저 컨트롤러 및 dto dao xml 파일 작성

---
+++ backend/src/main/java/com/activityx/allei/dao/UserDao.java
@@ -0,0 +1,35 @@
+package com.activityx.allei.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.activityx.allei.dto.User;
+
+// CRUD
+@Mapper
+public interface UserDao {
+	// create
+	public int create(User user);
+
+	// read
+	public List<User> readAll();
+
+	public User findById(int id);
+
+	public User findByName(String name);
+
+	public List<User> findlistByName(String name);
+
+	public User findByEmail(String email);
+
+	public List<User> findlistByEmail(String email);
+
+	public User findByKey(long key);
+
+	// update
+	public int update(User user);
+
+	// delete
+	public int delete(int id);
+}
