69d1fae3fa3f0f0c138152ac4c6f7c93d84c3335
Junsoo Park <junsoodark@naver.com>
2020-10-30 17:10:52 +0900

    [modify] 유저 컨트롤러

--- backend/src/main/java/com/activityx/allei/controller/UserController.java
+++ backend/src/main/java/com/activityx/allei/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.activityx.allei.controller;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -39,15 +40,155 @@ public class UserController {
     @ApiOperation(value = "유저 생성", response = BasicResponse.class)
     @PostMapping("create")
     public ResponseEntity<BasicResponse> createUser(@RequestBody User user) {
-        logger.debug("User DB test ; createUser - 호출");
+        logger.debug("User DB test : createUser - 호출");
         final BasicResponse result = new BasicResponse();
-        if(service.joinUser(user)){
+        if (service.joinUser(user)) {
             result.status = true;
-        }else{
+        } else {
             result.status = false;
             result.msg = "유저 생성에 실패하였습니다.";
         }
         return new ResponseEntity<BasicResponse>(result, HttpStatus.OK);
     }
-    
+
+    @ApiOperation(value = "유저 정보 수정", response = BasicResponse.class)
+    @PutMapping("modify")
+    public ResponseEntity<BasicResponse> modifyUser(@RequestBody User user) {
+        logger.debug("User DB test : modifyUser - 호출");
+        final BasicResponse result = new BasicResponse();
+        if (service.modifyUser(user)) {
+            result.status = true;
+        } else {
+            result.status = false;
+            result.msg = "유저 정보 수정에 실패하였습니다.";
+        }
+        return new ResponseEntity<BasicResponse>(result, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "유저 정보 삭제", response = BasicResponse.class)
+    @DeleteMapping("delete/{id}")
+    public ResponseEntity<BasicResponse> deleteUser(@PathVariable int id) {
+        logger.debug("User DB test : deleteUser - 호출");
+        final BasicResponse result = new BasicResponse();
+        if (service.deleteUser(id)) {
+            result.status = true;
+        } else {
+            result.status = false;
+            result.msg = "유저 정보 삭제에 실패하였습니다.";
+        }
+        return new ResponseEntity<BasicResponse>(result, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "모든 유저 정보 조회", response = BasicResponse.class)
+    @GetMapping("all")
+    public ResponseEntity<BasicResponse> readAllUser() {
+        logger.debug("User DB test : readAllUser - 호출");
+        final BasicResponse result = new BasicResponse();
+        List<User> data = service.findAll();
+        if (data != null) {
+            result.status = true;
+            result.data = data;
+        } else {
+            result.status = false;
+            result.msg = "유저 정보가 없습니다.";
+        }
+        return new ResponseEntity<BasicResponse>(result, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "유저 ID로 정보 조회", response = BasicResponse.class)
+    @GetMapping("findById/{id}")
+    public ResponseEntity<BasicResponse> readUserById(@PathVariable int id) {
+        logger.debug("User DB test : readUserById - 호출");
+        final BasicResponse result = new BasicResponse();
+        User data = service.findById(id);
+        if (data != null) {
+            result.status = true;
+            result.data = data;
+        } else {
+            result.status = false;
+            result.msg = "유저 정보가 없습니다.";
+        }
+        return new ResponseEntity<BasicResponse>(result, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "유저 Name으로 정보 조회", response = BasicResponse.class)
+    @GetMapping("findByName/{name}")
+    public ResponseEntity<BasicResponse> readUserByName(@PathVariable String name) {
+        logger.debug("User DB test : readUserByName - 호출");
+        final BasicResponse result = new BasicResponse();
+        User data = service.findByName(name);
+        if (data != null) {
+            result.status = true;
+            result.data = data;
+        } else {
+            result.status = false;
+            result.msg = "유저 정보가 없습니다.";
+        }
+        return new ResponseEntity<BasicResponse>(result, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "유저 Name으로 list 조회", response = BasicResponse.class)
+    @GetMapping("findlistByName/{name}")
+    public ResponseEntity<BasicResponse> readUserListByName(@PathVariable String name) {
+        logger.debug("User DB test : readUserListByName - 호출");
+        final BasicResponse result = new BasicResponse();
+        List<User> data = service.findlistByName(name);
+        if (data != null) {
+            result.status = true;
+            result.data = data;
+        } else {
+            result.status = false;
+            result.msg = "유저 정보가 없습니다.";
+        }
+        return new ResponseEntity<BasicResponse>(result, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "유저 Email로 정보 조회", response = BasicResponse.class)
+    @GetMapping("findByEmail/{email}")
+    public ResponseEntity<BasicResponse> readUserByEmail(@PathVariable String email) {
+        logger.debug("User DB test : readUserByEmail - 호출");
+        final BasicResponse result = new BasicResponse();
+        User data = service.findByName(email);
+        if (data != null) {
+            result.status = true;
+            result.data = data;
+        } else {
+            result.status = false;
+            result.msg = "유저 정보가 없습니다.";
+        }
+        return new ResponseEntity<BasicResponse>(result, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "유저 Email로 list 조회", response = BasicResponse.class)
+    @GetMapping("findlistByEmail/{email}")
+    public ResponseEntity<BasicResponse> readUserListByEmail(@PathVariable String email) {
+        logger.debug("User DB test : readUserListByEmail - 호출");
+        final BasicResponse result = new BasicResponse();
+        List<User> data = service.findlistByName(email);
+        if (data != null) {
+            result.status = true;
+            result.data = data;
+        } else {
+            result.status = false;
+            result.msg = "유저 정보가 없습니다.";
+        }
+        return new ResponseEntity<BasicResponse>(result, HttpStatus.OK);
+    }
+
+    @ApiOperation(value = "유저 key값으로 정보 조회", response = BasicResponse.class)
+    @GetMapping("findByKey/{key}")
+    public ResponseEntity<BasicResponse> readUserByKey(@PathVariable long key) {
+        logger.debug("User DB test : readUserByKey - 호출");
+        final BasicResponse result = new BasicResponse();
+        User data = service.findByKey(key);
+        if (data != null) {
+            result.status = true;
+            result.data = data;
+        } else {
+            result.status = false;
+            result.msg = "유저 정보가 없습니다.";
+        }
+        return new ResponseEntity<BasicResponse>(result, HttpStatus.OK);
+    }
+
 }
