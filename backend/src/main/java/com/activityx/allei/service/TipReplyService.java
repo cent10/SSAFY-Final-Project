888ce4121b331d74ba5e9e98d056e802f7f227da
김태훈 <ktahu2@naver.com>
2020-11-04 01:02:41 +0900

    [add] S03P31A210-45, S03P31A210-46, S03P31A210-47 : 팁게시판 댓글 CRUD

---
+++ backend/src/main/java/com/activityx/allei/service/TipReplyService.java
@@ -0,0 +1,21 @@
+package com.activityx.allei.service;
+
+import java.util.ArrayList;
+
+import com.activityx.allei.dto.TipReplyDto;
+
+public interface TipReplyService {
+
+	//특정 글에 대한 모든 댓글 가져오기
+	ArrayList<TipReplyDto> getAllReplies(int tipId);
+	
+	//댓글 쓰기
+	boolean addReply(TipReplyDto tipReply);
+	
+	//댓글 수정
+	boolean modifyReply(TipReplyDto tipReply);
+	
+	//댓글 삭제
+	boolean deleteReply(int id);
+		
+}
