ca0b62c21cb99bc573ee103e8d9de6156ec059f8
Kwon <klw95@naver.com>
2020-11-08 20:10:40 +0900

    [modify] S03P31A210-68 : 시작날짜 끝날짜로 변경

--- frontend/src/components/Purchase.vue
+++ frontend/src/components/Purchase.vue
@@ -1,46 +1,60 @@
 <template>
-  <b-container fluid>
-    <b-row align-h="center">
-      <h1>결제페이지</h1>
-    </b-row>
-    <b-row align-h="center">
-      <div>
-        <b-calendar block hide-header  v-model="date"></b-calendar>
-      </div>
-    </b-row>
-    <b-row align-h="center">
-      <div>
-        <h3>구매 정보</h3>
-        <h5>예약날짜: {{date}}</h5>
-        <b-list-group>
-          <b-list-group-item v-for="(pl, i) in purchasingList" :key="i">
-            <table>
-              <tr>
-                <td>
-                  {{pl.name}}
-                </td>
-                <td>
-                  {{pl.buyNum}}개
-                </td>
-                <td>
-                  {{pl.price * pl.buyNum}}원
-                </td>
-              </tr>
-            </table>
-          </b-list-group-item>
-        </b-list-group>
-      </div>
-    </b-row>
-    <b-row align-h="center">
-      <div>
-        총 {{totalPrice}}원
-      </div>
-    </b-row>
-    <b-row align-h="center">
-      <b-button>카카오페이로 결제</b-button>
-      <b-button @click="back">뒤로가기</b-button>
-    </b-row>
-  </b-container>
+  <div>
+  <div class="pay-info">
+    <b-container fluid>
+      <!-- <b-row align-h="center">
+        <h1>결제페이지</h1>
+      </b-row> -->
+      <b-row align-h="center" class="pay-row">
+        <h2>구매 정보</h2>
+      </b-row>
+      <b-row align-h="center" class="pay-row">
+        <div>
+          <label for="start-date">시작 날짜</label>
+          <b-form-datepicker hide-header id="start-date" v-model="startDate"></b-form-datepicker>
+        </div>
+        <div>
+          <label for="end-date">끝 날짜</label>
+          <b-form-datepicker :disabled="startDate === ''" :date-disabled-fn="dateDisabled" hide-header id="end-date" v-model="endDate"></b-form-datepicker>
+        </div>
+      </b-row>
+      <b-row align-h="center" class="pay-row">
+          <h5>예약날짜</h5>
+          {{startDate}} ~ {{endDate}}
+      </b-row>
+      <b-row align-h="center" class="pay-row">
+        <div>
+          <b-list-group>
+            <b-list-group-item v-for="(pl, i) in purchasingList" :key="i">
+              <table>
+                <tr>
+                  <td>
+                    {{pl.name}}
+                  </td>
+                  <td>
+                    {{pl.buyNum}}개
+                  </td>
+                  <td>
+                    {{pl.price * pl.buyNum}}원
+                  </td>
+                </tr>
+              </table>
+            </b-list-group-item>
+          </b-list-group>
+        </div>
+      </b-row>
+      <b-row align-h="center" class="pay-row">
+        <div>
+          총 {{totalPrice}}원
+        </div>
+      </b-row>
+      <!-- <b-row align-h="center">
+      </b-row> -->
+    </b-container>
+  </div>
+        <b-button>카카오페이로 결제</b-button>
+        <b-button @click="back">뒤로가기</b-button>
+  </div>
 </template>
 
 <script>
@@ -54,7 +68,7 @@ export default {
     value: {
     },
   },
-  data: function () {
+  data() {
     const now = new Date();
     return {
       isPurchasing: this.value,
@@ -62,6 +76,8 @@ export default {
       context: null,
       purchasingList: this.pl,
       totalPrice: 0,
+      startDate: "",
+      endDate: "",
     }
   },
   created() {
@@ -75,10 +91,26 @@ export default {
     },
     onContext(ctx) {
       this.context = ctx
+    },
+    dateDisabled(ymd) {
+      return ymd < this.startDate;
     }
+  },
+  watch: {
+    startDate (to){
+      this.endDate = to;
+    },
   }
 };
 </script>
 
 <style scoped>
+.pay-info {
+  margin: auto;
+  width: 50%;
+  background-color:lightgray;
+}
+.pay-row {
+  padding: 3%;
+}
 </style>
