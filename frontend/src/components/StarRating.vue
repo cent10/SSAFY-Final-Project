acacf9f06a971662469ee37301af657c1763136e
Kwon <klw95@naver.com>
2020-11-08 14:03:13 +0900

    [add] S03P31A210-63 : 평점 추가

---
+++ frontend/src/components/StarRating.vue
@@ -0,0 +1,36 @@
+<template>
+  <span>
+    <i v-for="(n, i) in maxStars"
+        :key="i"
+       :class="getClass(n)" 
+       :style="getStyle(n)"></i>
+  </span>
+  
+</template>
+
+<script>
+export default {
+    name: "StarRating",
+    props:{
+        value:{type: Number, default: 0},
+        maxStars: {type: Number, default: 5},
+        starredColor: {type: String, default: "orange"},
+        blankColor: {type: String, default: "darkgray"},
+    },
+    methods:{
+        getClass(n){
+            return {
+                "fa": true,
+                "fa-star": n <= this.value,
+                "fa-star-o": n > this.value,
+                "fa-2x": true,
+            }
+        },
+        getStyle(n){
+            return {
+                color: n <= this.value ? this.starredColor : this.blankColor
+            }
+        }
+    }
+}
+</script>
