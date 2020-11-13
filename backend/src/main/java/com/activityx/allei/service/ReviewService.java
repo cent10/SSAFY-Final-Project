8c17073c249fd8adbc3affc18f824368e993123c
박진 <cent10@naver.com>
2020-11-13 10:06:05 +0900

    [add] S03P31A210-136 : 업체 id로 후기 리스트 조회하는 기능 추가

--- backend/src/main/java/com/activityx/allei/service/ReviewService.java
+++ backend/src/main/java/com/activityx/allei/service/ReviewService.java
@@ -1,5 +1,7 @@
 package com.activityx.allei.service;
 
+import java.util.List;
+
 import com.activityx.allei.dto.ReviewDto;
 
 public interface ReviewService {
@@ -9,6 +11,9 @@ public interface ReviewService {
 	
 	// 후기 조회
 	ReviewDto read(int id);
+	
+	// 후기 리스트 조회 (업체 번호로 후기 리스트 조회)
+	List<ReviewDto> readReviews(int shop);
 	
 	// 후기 수정
 	boolean update(ReviewDto reviewDto);
